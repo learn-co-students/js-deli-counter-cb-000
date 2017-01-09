f3c40ef4e5325358ee129d6aa3532210e4db92d7
dustinlikens <dustinlikens@icloud.com>
2017-01-08 21:06:05 -0800

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length == 0){
-    return"The line is currently empty."
+    return "The line is currently empty."
   }else{
     var string = "The line is currently: "
     for(let i=0;i<katzDeliLine.length;i++){
